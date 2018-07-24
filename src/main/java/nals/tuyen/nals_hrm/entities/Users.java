86a0c0b6354955003ac118ff67c0459463be16c6
thanhtuyenit <nguyenthithanhtuyen.dr@gmail.com>
2018-07-24 13:48:39 +0700

    version1.0

--- src/main/java/nals/tuyen/nals_hrm/entities/Users.java
+++ src/main/java/nals/tuyen/nals_hrm/entities/Users.java
@@ -1,4 +1,18 @@
 package nals.tuyen.nals_hrm.entities;
 
-public class Users {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "employees")
+public class Users implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue
+  @Column(name = "id", nullable = false)
+  int idUser;
+
+
 }
