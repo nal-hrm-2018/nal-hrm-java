5ed8e928f003ebf3b766e67b3b016eb35ceccea1
Nguyễn Thị Thanh Tuyền <nguyenthithanhtuyen.dr@gmail.com>
2018-10-12 08:59:41 +0700

    done

--- src/main/java/nals/hrm/api_nals_hrm/repository/AbsenceStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package nals.hrm.api_nals_hrm.repository;
-
-import nals.hrm.api_nals_hrm.entities.AbsenceStatus;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AbsenceStatusRepository extends CrudRepository<AbsenceStatus, Integer> {
-}
